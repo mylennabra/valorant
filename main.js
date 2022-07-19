1da62af28aa489bd533a2b2eb6598413cf4c2dc7
mylennabra <mylenna.bra@gmail.com>
2022-07-19 13:34:33 -0300

    feat(Visibilidade da tela inicial ao abrir offcanvas)

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ if(window.screen.width < 1000){
     document.querySelectorAll(".xyz-funcoes").forEach(element => {element.classList.add("flex-column");});
 }
 
-if(document.querySelector(".offcanvas").classList.contains('show')){
-    document.querySelector(".xyz-home").classList.add("d-none");
+let decide = document.querySelector(".offcanvas"), btntoggles = document.getElementsByClassName("btntoggle"), home = document.querySelector(".xyz-home");
+
+for (const btntoggle of btntoggles) {
+    btntoggle.addEventListener('click', function onClick() {
+        document.querySelector(".offcanvas").classList.contains('show') ? home.classList.add('d-none') : home.classList.remove('d-none');
+    })
 }
